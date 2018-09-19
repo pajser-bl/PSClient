c3a4fbee9b0d4e8aa7cb45fd697e387e882a7fb7
Pikooo <Pikooo@PC>
2018-09-19 17:57:34 +0200

    ASD

--- PSClient/src/client/Client.java
+++ PSClient/src/client/Client.java
@@ -6,19 +6,35 @@ import java.util.Properties;
 public class Client {
 	
 	public static void main(String ars[]) {
-		//setup...
 		try {
 			Properties props = new Properties();
 			FileInputStream fIS = new FileInputStream("client.conf");
 			props.load(fIS);
-			ClientCommunication clientCommunication = new ClientCommunication(props.getProperty("client.server_ip"),Integer.parseInt(props.getProperty("client.server_port")));
+			ClientCommunication clientCommunication = new ClientCommunication(props.getProperty("client.server_ip"),
+																			  Integer.parseInt(props.getProperty("client.server_port")));
 			RequestFunctionality requestFunctionality = new RequestFunctionality(clientCommunication);
 		
-			for(String s:requestFunctionality.login("a","a"))
+			for(String s:requestFunctionality.login("a", "a"))
+				System.out.println(s);
+			/*for(String s:requestFunctionality.newCredentials("3", "b", "b"))
+				//System.out.println(s);
+			for(String s:requestFunctionality.updateCredentials("2", "c", "c"))
+				System.out.println(s);
+			for(String s:requestFunctionality.deleteCredentials("2"))
+				System.out.println(s);
+			for(String s:requestFunctionality.newUser("Zeljka", "Cvijanovic", "1967-08-02", "Administrator", "Prevelika"))
+				System.out.println(s);*/
+			//for(String s:requestFunctionality.updateUser("1", "Darken", "Andric", "1987-01-01", "Supervizor", "Kaskader"))
+				//System.out.println(s);
+			//for(String s:requestFunctionality.newCredentials("2", "b", "b"))
+				//System.out.println(s);
+			for(String s:requestFunctionality.deleteUser("2"))
+				System.out.println(s);
+			for(String s:requestFunctionality.logout())
 				System.out.println(s);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}	
+		}
 	}
 }
--- PSClient/src/client/RequestFunctionality.java
+++ PSClient/src/client/RequestFunctionality.java
@@ -10,11 +10,71 @@ public class RequestFunctionality {
 		this.clientCommunication=clientCommunication;
 	}
 	
-	public ArrayList<String> login(String username,String password){
+	public ArrayList<String> login(String username,String password) {
 		ArrayList<String> arguments = new ArrayList<>();
 		arguments.add(username);
 		arguments.add(password);
 		Request request = new Request("LOGIN", arguments);
+		return clientCommunication.sendRequest(request);
+	}
+	
+	public ArrayList<String> logout() {
+		Request request = new Request("LOGOUT", null);
+		return clientCommunication.sendRequest(request);
+	}
+	
+	public ArrayList<String> newUser(String name, String surname, String dateOfBirth, String type, String qualification){
+		ArrayList<String> arguments = new ArrayList<>();
+		arguments.add(name);
+		arguments.add(surname);
+		arguments.add(dateOfBirth);
+		arguments.add(type);
+		arguments.add(qualification);
+		Request request = new Request("NEW USER", arguments);
+		return clientCommunication.sendRequest(request);
+	}
+	
+	public ArrayList<String> updateUser(String userID, String name, String surname, String dateOfBirth, String type, String qualification){
+		ArrayList<String> arguments = new ArrayList<>();
+		arguments.add(userID);
+		arguments.add(name);
+		arguments.add(surname);
+		arguments.add(dateOfBirth);
+		arguments.add(type);
+		arguments.add(qualification);
+		Request request = new Request("UPDATE USER", arguments);
+		return clientCommunication.sendRequest(request);
+	}
+	
+	public ArrayList<String> deleteUser(String userID){
+		ArrayList<String> arguments = new ArrayList<>();
+		arguments.add(userID);
+		Request request = new Request("DELETE USER", arguments);
+		return clientCommunication.sendRequest(request);
+	}
+	
+	public ArrayList<String> newCredentials(String userID, String username, String password){
+		ArrayList<String> arguments = new ArrayList<>();
+		arguments.add(userID);
+		arguments.add(username);
+		arguments.add(password);
+		Request request = new Request("NEW CREDENTIALS", arguments);
+		return clientCommunication.sendRequest(request);
+	}
+	
+	public ArrayList<String> updateCredentials(String userID, String username, String password){
+		ArrayList<String> arguments = new ArrayList<>();
+		arguments.add(userID);
+		arguments.add(username);
+		arguments.add(password);
+		Request request = new Request("UPDATE PASSWORD", arguments);
+		return clientCommunication.sendRequest(request);
+	}
+	
+	public ArrayList<String> deleteCredentials(String userID){
+		ArrayList<String> arguments = new ArrayList<>();
+		arguments.add(userID);
+		Request request = new Request("DELETE CREDENTIALS", arguments);
 		return clientCommunication.sendRequest(request);
 	}
 }
