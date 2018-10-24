0d17da70d20512015ece14dee98c20846ade9e36
Pikooo <Pikooo@PC>
2018-10-24 15:54:14 +0200

    1

---
+++ PSClient/src/control/LoginController.java
@@ -0,0 +1,29 @@
+package control;
+	import javafx.scene.control.TextField;
+	import javafx.scene.layout.AnchorPane;
+	import javafx.scene.layout.Pane;
+	import javafx.stage.Stage;
+
+	import java.awt.Button;
+	import java.awt.event.ActionEvent;
+	import java.io.IOException;
+	import java.net.URL;
+	import java.sql.Date;
+	import java.text.DateFormat;
+	import java.text.SimpleDateFormat;
+	import java.util.ResourceBundle;
+
+	import javafx.fxml.FXML;
+	import javafx.fxml.FXMLLoader;
+import client.Client;
+
+public class LoginController {
+
+	Client login;
+		
+		/*@FXML
+		private void goLogin() throws IOException {
+			main.showLogin();
+		}*/
+
+	}
