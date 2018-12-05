de3656fd802b70d01d686a1bafe87ddd9b6ebac0
Bojana <Bojana@Bojana>
2018-12-05 15:01:26 +0100

    NotificationForm
    	1. Dodana NotificationForm
    	2. NotificationControll nije dobar - sadrzi kod za animaciju.

---
+++ PSClient/src/control/NotificationController.java
@@ -0,0 +1,58 @@
+//ispod je kod koji omogucava da ivice svijetle
+//treba da se uklone i ivice od prozora
+
+package control;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.animation.FadeTransition;
+import javafx.animation.ParallelTransition;
+import javafx.animation.Timeline;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.util.Duration;
+
+
+public class NotificationController implements Initializable{
+
+	Notification upozorenje;
+	
+	@FXML
+	private Node donja_ivica;
+	@FXML
+	private Node gornja_ivica;
+	
+
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		
+		FadeTransition fadeOut = new FadeTransition(Duration.seconds(2.0), donja_ivica);
+		fadeOut.setFromValue(1.0);
+		fadeOut.setToValue(0.0);
+		fadeOut.setAutoReverse(true);
+		//fadeOut.setCycleCount(FadeTransition.INDEFINITE);
+		
+		FadeTransition fadeIn = new FadeTransition(Duration.seconds(2.0), gornja_ivica);
+		fadeIn.setFromValue(0.0);
+			fadeIn.setToValue(1.0);
+		fadeIn.setAutoReverse(true);
+		//fadeIn.setCycleCount(FadeTransition.INDEFINITE);
+	
+		//fadeOut.play();
+		//fadeIn.play();
+		
+		ParallelTransition parallelTransition = new ParallelTransition(fadeOut,fadeIn);
+		parallelTransition.setAutoReverse(true);
+		parallelTransition.setCycleCount(Timeline.INDEFINITE);
+        parallelTransition.play();
+		
+	}	
+	
+}
+
+package control;
+
+public class NotificationController {
+
+}
